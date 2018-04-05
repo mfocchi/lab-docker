0f01857c879aa8276fc32758168fbfc6d7d55ea3
Gennaro Raiola <gennaro.raiola@iit.it>
2018-04-05 10:47:06 +0200

    Try to generalize the docker build

--- build_docker.sh
+++ build_docker.sh
@@ -1,28 +1,18 @@
 #!/usr/bin/env bash
 
 # Check args
+if [ "$#" -ne 2 ]; then
+  echo "usage: ./build_docker.sh ID CACHE_ON"
+  exit
+fi
 
-CACHE_ON=false
+ID=$1
+CACHE_ON=$2
 
-# Get this script's path
-#pushd `dirname $0` > /dev/null
-#SCRIPTPATH=`pwd`
-#popd > /dev/null
-
-#SSH_PRIVATE_KEY=`cat $2/id_rsa | cut -d'-' -f 2 | cut -d'-' -f 1`
-#SSH_PRIVATE_KEY="aaa"
-#SSH_PUBLIC_KEY="bbb"
-
-# Prepare the context
-#cp $1/id_rsa ./dev/
-#cp $1/id_rsa.pub ./dev/
-
-#SSH_PRIVATE_KEY=`cat $2/id_rsa`
-#SSH_PUBLIC_KEY=`cat $2/id_rsa.pub`
-
-# Build the docker image
-#docker build --build-arg GITLAB_IP=172.31.1.50 --build-arg SSH_PUBLIC_KEY=$SSH_PUBLIC_KEY --build-arg SSH_PRIVATE_KEY=$SSH_PRIVATE_KEY -t $1 .
-
-
-docker build -f ./env/Dockerfile-env --build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" --build-arg SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" -t "dls-env" ./env/
-
+if $CACHE_ON; then
+	echo "CACHE ON"
+	docker build -f ./$ID/Dockerfile-$ID --network=host --build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" --build-arg SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" -t dls-$ID ./$ID/
+else
+	echo "CACHE OFF"
+	docker build -f ./$ID/Dockerfile-$ID --network=host --no-cache --build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" --build-arg SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" -t dls-$ID ./$ID/
+fi

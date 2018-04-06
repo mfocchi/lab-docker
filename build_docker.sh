efc1587ba68f5470d688346082231d6eb34f562d
Gennaro Raiola <gennaro.raiola@iit.it>
2018-04-06 17:25:05 +0200

    Try to parallelize builds

--- build_docker.sh
+++ build_docker.sh
@@ -2,17 +2,17 @@
 
 # Check args
 if [ "$#" -ne 2 ]; then
-  echo "usage: ./build_docker.sh ID CACHE_ON"
+  echo "usage: ./build_docker.sh NAME CACHE_ON"
   exit
 fi
 
-ID=$1
+NAME=$1
 CACHE_ON=$2
 
 if $CACHE_ON; then
 	echo "CACHE ON"
-	docker build -f ./$ID/Dockerfile-$ID --network=host --build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" --build-arg SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" -t dls-$ID ./$ID/
+	docker build -f ./$NAME/Dockerfile --network=host --build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" --build-arg SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" -t $NAME ./$NAME/
 else
 	echo "CACHE OFF"
-	docker build -f ./$ID/Dockerfile-$ID --network=host --no-cache --build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" --build-arg SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" -t dls-$ID ./$ID/
+	docker build -f ./$NAME/Dockerfile --network=host --no-cache --build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" --build-arg SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" -t $NAME ./$NAME/
 fi

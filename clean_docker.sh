e0b6300840759d38e6b486ef8741a4c1aa9f135a
dlsuser <dlsuser@server-dev>
2018-07-10 10:28:14 +0200

    Clean registry

--- clean_docker.sh
+++ clean_docker.sh
@@ -10,4 +10,5 @@ docker volume rm $(docker volume ls -qf dangling=true)
 # Images
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
-
+# Registry
+docker exec $(docker ps -f "name=registry" --format "{{.ID}}") registry garbage-collect /etc/docker/registry/config.yml

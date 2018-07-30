bfdc2a67e7ed7c88a11b393eba8dc62ae566573c
Gennaro Raiola <gennaro.raiola@iit.it>
2018-07-30 17:07:40 +0200

    Run docker with your ssh key, yuhu

--- run_docker.sh
+++ run_docker.sh
@@ -2,8 +2,8 @@
 
 # Check args
 if [ "$#" -ne 1 ]; then
-  echo "usage: ./run.sh IMAGE_NAME"
-  exit
+  echo "usage: ./run.sh [IMAGE_NAME=server-dev:5000/dls-dev]"
+  IMAGE_NAME="server-dev:5000/dls-dev"
 fi
 
 # Get this script's path
@@ -17,4 +17,5 @@ set -e
 xhost +local:docker
 
 # Run the container with shared X11
-docker run --device=/dev/dri:/dev/dri --net=host -e "QT_X11_NO_MITSHM=1" -e SHELL -e DISPLAY -e DOCKER=1 -v "$HOME:$HOME:rw" -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" -it $1 $SHELL
+#--entrypoint "eval $(/usr/bin/ssh-agent -s) /usr/bin/ssh-add /home/`whoami`/.ssh/id_rsa"
+docker run --device=/dev/dri:/dev/dri --net=host -e "QT_X11_NO_MITSHM=1" -e SHELL -e DISPLAY -e DOCKER=1 -v "$HOME:$HOME:rw" -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" -it $IMAGE_NAME $SHELL -c "eval \`/usr/bin/ssh-agent -s\`; /usr/bin/ssh-add /home/`whoami`/.ssh/id_rsa; export HOME=$HOME; cd $HOME; source /opt/ros/dls-distro/setup.bash; exec /bin/bash"

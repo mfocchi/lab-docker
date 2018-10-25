ae5e7bd9a789b1cffce0aba0f7c0d9a4c54b0c8b
dlsuser@server-dev <dlsuser@server-dev.iit.it>
2018-10-25 15:34:42 +0200

    Remove registry if it exists and create it back

--- create_registry.sh
+++ create_registry.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
+
 REGISTRY_NAME=registry
 
-if [ ! "$(docker ps -q -f name=$REGISTRY_NAME)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=$REGISTRY_NAME)" ]; then
-        # cleanup
-        docker rm $REGISTRY_NAME
-    fi
-    # run the registry
-    docker run -d -p 5000:5000 --restart=always --name $REGISTRY_NAME -v ./config/registry_config.yml:/etc/docker/registry/config.yml registry:2
+if [ "$(docker ps -q -f name=$REGISTRY_NAME)" ]; then
+    # stop the registry
+    docker stop $REGISTRY_NAME
+    # cleanup
+    docker rm $REGISTRY_NAME
 fi
-
-
+# run the registry
+docker run -d -p 5000:5000 --restart=always --name $REGISTRY_NAME -v $(pwd)/config/registry_config.yml:/etc/docker/registry/config.yml registry:2

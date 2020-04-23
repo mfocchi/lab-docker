f26ea1fb73fd96d39bfc9cb3dec8439f91ab2f2b
Geoff Fink <geoff.fink@iit.it>
2020-04-23 19:25:01 +0200

    update fast rtps install script

--- scripts/install_fastrtps.sh
+++ scripts/install_fastrtps.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y libasio-dev libtinyxml2-dev cmake
+# Inlcude common functions
 
+echo "Install fastrtps base dependencies"
+echo "========================================================================="
+apt-get install -y libasio-dev libtinyxml2-dev cmake
+
+echo "Install FastCDR"
+echo "========================================================================="
 git clone https://github.com/eProsima/Fast-CDR.git
 mkdir Fast-CDR/build && cd Fast-CDR/build
 cmake ..
 cmake --build . --target install
 cd ../..
 
+echo "Install foonathan memory"
+echo "========================================================================="
 git clone https://github.com/eProsima/foonathan_memory_vendor.git
 cd foonathan_memory_vendor
 mkdir build && cd build
@@ -16,8 +23,18 @@ cmake ..
 cmake --build . --target install
 cd ../..
 
+echo "Instal FastRTPS"
+echo "========================================================================="
 git clone https://github.com/eProsima/Fast-RTPS.git
 mkdir Fast-RTPS/build && cd Fast-RTPS/build
 cmake ..
 cmake --build . --target install
 cd ../..
+
+echo "Install fastrtpsgen"
+echo "========================================================================="
+git clone --recursive https://github.com/eProsima/Fast-RTPS-Gen.git
+cd Fast-RTPS-Gen
+gradle assemble
+cp scripts/fastrtpsgen /usr/local/bin/fastrtpsgen
+cd ..

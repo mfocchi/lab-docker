0727f3f645695d651c5077288d2674d557947d6e
Geoff Fink <geoff.fink@iit.it>
2020-04-23 11:07:33 +0200

    Add pandoc and doxygen

--- scripts/install_linux_tools.sh
+++ scripts/install_linux_tools.sh
@@ -6,7 +6,7 @@ LIST=linux_tools.txt
 PKGS=`cat $FOLDER/$LIST | grep -v \#`
 
 for PKG in $PKGS; do
-  if apt-cache show $PKG 1>/dev/null 2>&1; then`
+  if apt-cache show $PKG 1>/dev/null 2>&1; then
     sudo apt-get -y install $PKG
   else
     echo ERROR: $PKG does not exist!

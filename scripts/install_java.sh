807d4c661b8bbc3642da971ed07b792af69363b3
Geoff Fink <geoff.fink@iit.it>
2020-04-27 18:47:03 +0200

    a

--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -9,8 +9,8 @@ apt-get update
 mkdir -p /var/cache/oracle-jdk11-installer-local
 cd /var/cache/oracle-jdk11-installer-local
 wget http://server-ubuntu18/java/jdk-11.0.7_linux-x64_bin.tar.gz
-echo "oracle-java11-installer-local shared/accepted-oracle-license-v1-2 select true" | sudo debconf-set-selections
-echo "oracle-java11-installer shared/accepted-oracle-license-v1-2 select true" | sudo debconf-set-selections
+echo "oracle-java11-installer-local shared/accepted-oracle-license-v1-2 select true" | debconf-set-selections
+echo "oracle-java11-installer shared/accepted-oracle-license-v1-2 select true" | debconf-set-selections
 
 apt-get -y install oracle-java11-installer-local
 apt-get -y install oracle-java11-set-default-local

41c1e9515283fa5339735f3212e41213a66dea9d
gfink <geoff.fink@iit.it>
2020-12-07 12:07:17 +0100

    fix: kitware disabled http, so switching to https (+ proxy pass-through.  Also bumping cmake version

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -2,9 +2,10 @@
 
 
 # Add CMake PPA
-wget -O - http://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | apt-key add -
-apt-add-repository 'deb http://apt.kitware.com/ubuntu/ xenial main'
+echo 'Acquire::HTTPS::Proxy::apt.kitware.com "DIRECT";' | tee -a /etc/apt/apt.conf.d/proxy \
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | apt-key add -
+apt-add-repository 'deb https://apt.kitware.com/ubuntu/ xenial main'
 rm -rf /var/lib/apt/lists/*
 apt-get update
-apt-get install --no-install-recommends -y cmake-data=3.18.0-0kitware1 cmake=3.18.0-0kitware1 cmake-curses-gui=3.18.0-0kitware1 cmake-qt-gui=3.18.0-0kitware1
+apt-get install --no-install-recommends -y cmake-data=3.19.1-0kitware1 cmake=3.19.1-0kitware1 cmake-curses-gui=3.19.1-0kitware1 cmake-qt-gui=3.19.1-0kitware1
 rm -rf /var/lib/apt/lists/*

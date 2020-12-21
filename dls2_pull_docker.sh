838a2316a30d9619842df85686cf770357ed7025
gfink <geoff.fink@iit.it>
2020-12-21 22:40:36 +0100

    remove old pull scripts

--- dls2_pull_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker pull server-harbor:80/dls/dls-env-nvidia:latest
-docker pull server-harbor:80/dls/dls-dev-nvidia:latest
-docker pull server-harbor:80/dls2/dls2-framework-dev-nvidia:latest
-docker pull server-harbor:80/dls2/dls2-plugin-dev-nvidia:latest
-docker pull server-harbor:80/dls2/dls2-operator-nvidia:latest
-docker system prune -f
--- dls2_pull_docker_nvidia.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker pull server-gitlab-runner:5000/dls2-env-nvidia:latest
-docker pull server-gitlab-runner:5000/dls2-framework-dev-nvidia:latest
-docker pull server-gitlab-runner:5000/dls2-plugin-dev-nvidia:latest
-docker pull server-gitlab-runner:5000/dls2-operator-nvidia:latest
-docker pull server-gitlab-runner:5000/dls2-plugin-trunk-nvidia-dev:latest
-
-docker system prune -f

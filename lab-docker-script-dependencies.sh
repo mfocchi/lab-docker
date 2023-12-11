7df676eda75314f77402254318ed4ba2998e996f
Michele Focchi <michele.focchi@unitn.it>
2023-12-11 15:51:02 +0100

    missing pip in mac

--- lab-docker-script-dependencies.sh
+++ lab-docker-script-dependencies.sh
@@ -9,6 +9,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
+	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 	brew install python3-pip python3-argcomplete
 	pip3 install --upgrade pip
 	pip3 install docker dockerpty dbus-python python-networkmanager argcomplete
